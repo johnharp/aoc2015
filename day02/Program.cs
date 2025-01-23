009c7e56d7a0a12d2cf48d85e3cc746aefd210db
John Harper <john@harper.link>
2025-01-23 10:54:21 -0600

    solve day02 and day03 for answers, but no visualization yet

---
+++ day02/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+
+namespace day02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string basedir = AppDomain.CurrentDomain.BaseDirectory;
+        string prefix = "../../../";
+        string filename = "input.txt";
+        Directory.SetCurrentDirectory(basedir);
+
+        long totalPaperNeeded = 0;
+        long totalRibbonNeeded = 0;
+
+        string[] lines = File.ReadAllLines($"{prefix}{filename}");
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            string line = lines[i];
+            string[] parts = line.Split('x');
+
+            Debug.Assert(parts.Length == 3, "All lines should have 3 parts");
+            int l = int.Parse(parts[0]);
+            int w = int.Parse(parts[1]);
+            int h = int.Parse(parts[2]);
+
+            totalPaperNeeded += paperNeeded(l, w, h);
+            totalRibbonNeeded += ribbonNeeded(l, w, h);
+        }
+
+        Console.WriteLine($"Part 1: {totalPaperNeeded}");
+        Console.WriteLine($"Part 2: {totalRibbonNeeded}");
+    }
+
+    static long paperNeeded(int l, int w, int h)
+    {
+        long a1 = l * w;
+        long a2 = w * h;
+        long a3 = h * l;
+
+        long amin = Math.Min(Math.Min(a1, a2), a3);
+        long area = 2 * a1 + 2 * a2 + 2 * a3 + amin;
+
+        return area;
+    }
+
+    static long ribbonNeeded(int l, int w, int h)
+    {
+        long p1 = 2*l + 2*w;
+        long p2 = 2*w + 2*h;
+        long p3 = 2*h + 2*l;
+
+        long perimMin = Math.Min(Math.Min(p1, p2), p3);
+
+        return perimMin + l*w*h;
+    }
+}
---
+++ day03/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Diagnostics;
+
+namespace day03;
+
+class Program
+{
+    private static Dictionary<(int, int), int> deliveries = new Dictionary<(int, int), int>();
+    private static Dictionary<(int, int), int> p2deliveries = new Dictionary<(int, int), int>();
+
+    static void Main(string[] args)
+    {
+        string basedir = AppDomain.CurrentDomain.BaseDirectory;
+        string prefix = "../../../";
+        string filename = "input.txt";
+        Directory.SetCurrentDirectory(basedir);
+
+        string[] lines = File.ReadAllLines($"{prefix}{filename}");
+        Debug.Assert(lines.Length == 1, "Expected only one line of input");
+        string line = lines[0];
+
+        (int, int) currentLocation = (0, 0);
+        (int, int) p2santa = (0, 0);
+        (int, int) p2robo = (0, 0);
+
+        incr(deliveries, currentLocation);
+        incr(p2deliveries, p2santa);
+        incr(p2deliveries, p2robo);
+
+
+        bool isSantaTurn = true;
+        foreach (char c in line)
+        {
+            currentLocation = move(c, currentLocation);
+            incr(deliveries, currentLocation);
+
+            if (isSantaTurn)
+            {
+                p2santa = move(c, p2santa);
+                incr(p2deliveries, p2santa);
+            }
+            else
+            {
+                p2robo = move(c, p2robo);
+                incr(p2deliveries, p2robo);
+            }
+            isSantaTurn = !isSantaTurn;
+        }
+
+        Console.WriteLine($"Part 1: {deliveries.Keys.Count}");
+        Console.WriteLine($"Part 2: {p2deliveries.Keys.Count}");
+    }
+
+    static void incr(Dictionary<(int, int), int> dict, (int, int) loc)
+    {
+        if (!dict.ContainsKey(loc))
+        {
+            dict[loc] = 0;
+        }
+
+        dict[loc]++;
+    }
+
+    static (int, int) move(char c, (int, int) loc)
+    {
+        (int, int) newLoc;
+
+        switch (c)
+        {
+            case '^':
+                newLoc = (
+                    loc.Item1,
+                    loc.Item2 - 1
+                );
+                break;
+            case 'v':
+                newLoc = (
+                    loc.Item1,
+                    loc.Item2 + 1
+                );
+                break;
+            case '<':
+                newLoc = (
+                    loc.Item1 - 1,
+                    loc.Item2
+                );
+                break;
+            case '>':
+                newLoc = (
+                    loc.Item1 + 1,
+                    loc.Item2
+                );
+                break;
+            default:
+                throw new Exception($"Unexpected input character: {c}");
+        }
+
+        return newLoc;
+    }
+}
